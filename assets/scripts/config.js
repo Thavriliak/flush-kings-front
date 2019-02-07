c61d5bee636c14a88bd172781630e2ee42a89a83
Thavriliak <Havriliakt@gmail.com>
2019-02-07 11:53:05 -0500

    Changed heroku database name

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://boiling-oasis-18540.herokuapp.com',
+  production: 'https://flushkings.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
